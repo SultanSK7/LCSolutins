7e1e9fb072d002838381d75caa477458ca09a90e
Shreyas Kulkarni <shreyas.kulkarni29@gmail.com>
2022-08-20 19:57:38 +0530

    Updated the code

--- LongestPrefix.java
+++ LongestPrefix.java
@@ -1,14 +1,13 @@
-public class LongestPrefix{
-
-	public static String Solution(String Strg){
-		String op="";
-		
-	} 
-
-	public static void main(String[] args) {
-		String[] Strg = {"flower","flask","float"}; 
-		String op="";
-		op = Solution(Strg);
-		System.out.println(op);
-	}
+class LongestPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        String prefix = strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+                System.out.println(prefix);
+            }
+        }
+        return prefix;
+    }
 }
